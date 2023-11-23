c61cc1d671f65848e5d593e521f870df5491b924
brbertholini <brbertholini@gmail.com>
2023-11-23 14:52:37 -0300

    cart validation

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,32 +1,45 @@
 import styled from 'styled-components'
-
+ 
 export const Container = styled.div`
     width: 100%;
     display: flex;
-    align-items: center;
-
+    flex-direction: column;
+ 
+    border: ${props => props.errors ? '1px solid red' : 'none'};
+ 
     background-color: #f5f5fa;
     color: black;
-
+ 
     margin-bottom: 8px;
     border-radius: 10px;
-
-
-    > input{
+ 
+    div {
+        width: 100%;
+        display: flex;
+        align-items: center;
+    }
+ 
+    > p {
+        padding-left: 15px;
+        padding-top: 15px;
+        color: red;
+    }
+ 
+    > div > input{
         height: 56px;
         width: 100%;
-
+ 
         padding: 12px;
-
+ 
         color: black;
         background: transparent;
         border: 0;
-
+ 
         &:placeholder{
             color: #424244;
         }
     }
-    > svg{
+    > div > svg{
             margin-left: 16px;
         }
 `;
