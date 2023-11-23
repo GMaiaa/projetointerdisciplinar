641dad892a89d9e5df3345d80f11553d8d11d4bb
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2023-11-23 08:51:39 -0300

    update

--- src/pages/ExchangeAndTrade/index.jsx
+++ src/pages/ExchangeAndTrade/index.jsx
@@ -9,11 +9,13 @@ export function ExchangeAndTrade() {
       <Header />
       <InformativePage
         title="Política de Troca / Estorno"
-        text="Produtos Danificados ou com Qualidade Comprometida: Caso receba um produto danificado ou com qualidade abaixo do esperado, pedimos gentilmente que entre em contato conosco em até 24 horas após o recebimento. Para agilizar o processo, solicitamos que nos forneça fotos que evidenciem o estado do produto.
+        text={(
+          <div>
+ "Produtos Danificados ou com Qualidade Comprometida: Caso receba um produto danificado ou com qualidade abaixo do esperado, pedimos gentilmente que entre em contato conosco em até 24 horas após o recebimento. Para agilizar o processo, solicitamos que nos forneça fotos que evidenciem o estado do produto.<br/><br/>
 
-Erros de Entrega: Em situações onde houve um equívoco em relação ao produto entregue, seja na quantidade, tipo ou variedade, entre em contato conosco imediatamente para providenciarmos a troca pelo item correto.
+Erros de Entrega: Em situações onde houve um equívoco em relação ao produto entregue, seja na quantidade, tipo ou variedade, entre em contato conosco imediatamente para providenciarmos a troca pelo item correto.<br/><br/>
 
-Troca por Arrependimento: Se por algum motivo você se arrependeu da compra, aceitaremos a devolução do produto desde que esteja em perfeitas condições, sem indícios de uso e na embalagem original. Nesse caso, solicitamos que entre em contato conosco em até 7 dias após o recebimento."
+Troca por Arrependimento: Se por algum motivo você se arrependeu da compra, aceitaremos a devolução do produto desde que esteja em perfeitas condições, sem indícios de uso e na embalagem original. Nesse caso, solicitamos que entre em contato conosco em até 7 dias após o recebimento."</div>)}
         image={ExchangeSVG}
         alt="Politica de trocas"
       />
